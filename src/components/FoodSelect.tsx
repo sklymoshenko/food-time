dc5505e06979c551a7ca1e1791432a9722e2a50e
Stanislav Klymoshenko <s.klymoshenko@gmail.com>
2022-09-18 16:03:50 +0200

    format foodselect

--- src/components/FoodSelect.tsx
+++ src/components/FoodSelect.tsx
@@ -1,4 +1,4 @@
-import { Autocomplete, TextField } from '@mui/material'
+import { Autocomplete, FilterOptionsState, TextField } from '@mui/material'
 import { createFilterOptions } from '@mui/material/Autocomplete'
 
 export type FoodOptionType = {
@@ -15,55 +15,60 @@ type FoodSelectProps = {
 }
 
 export const FoodSelect = ({ onFoodSelect, food }: FoodSelectProps) => {
+  const onSearchChange = (newValue: FoodOptionType | string | null) => {
+    if (typeof newValue === 'string') {
+      onFoodSelect({
+        title: newValue,
+      })
+    } else if (newValue && newValue.inputValue) {
+      // Create a new value from the user input
+      onFoodSelect({
+        title: newValue.inputValue,
+      })
+    } else {
+      onFoodSelect(newValue)
+    }
+  }
+
+  const filterOptions = (options: FoodOptionType[], params: FilterOptionsState<FoodOptionType>) => {
+    const filtered = filter(options, params)
+
+    const { inputValue } = params
+    // Suggest the creation of a new value
+    const isExisting = options.some((option) => inputValue === option.title)
+    if (inputValue !== '' && !isExisting) {
+      filtered.push({
+        inputValue,
+        title: `Add "${inputValue}"`,
+      })
+    }
+
+    return filtered
+  }
+
+  const getOptionLabel = (option: FoodOptionType | string) => {
+    if (typeof option === 'string') {
+      return option
+    }
+
+    if (option.inputValue) {
+      return option.inputValue
+    }
+
+    return option.title
+  }
+
   return (
     <Autocomplete
       value={food}
-      onChange={(event, newValue) => {
-        if (typeof newValue === 'string') {
-          onFoodSelect({
-            title: newValue,
-          })
-        } else if (newValue && newValue.inputValue) {
-          // Create a new value from the user input
-          onFoodSelect({
-            title: newValue.inputValue,
-          })
-        } else {
-          onFoodSelect(newValue)
-        }
-      }}
-      filterOptions={(options, params) => {
-        const filtered = filter(options, params)
-
-        const { inputValue } = params
-        // Suggest the creation of a new value
-        const isExisting = options.some((option) => inputValue === option.title)
-        if (inputValue !== '' && !isExisting) {
-          filtered.push({
-            inputValue,
-            title: `Add "${inputValue}"`,
-          })
-        }
-
-        return filtered
-      }}
+      onChange={(event, newValue) => onSearchChange(newValue)}
+      filterOptions={filterOptions}
       selectOnFocus
       clearOnBlur
       handleHomeEndKeys
       id='free-solo-with-text-demo'
       options={top100Films}
-      getOptionLabel={(option) => {
-        // Value selected with enter, right from the input
-        if (typeof option === 'string') {
-          return option
-        }
-        // Add "xxx" option created dynamically
-        if (option.inputValue) {
-          return option.inputValue
-        }
-        // Regular option
-        return option.title
-      }}
+      getOptionLabel={getOptionLabel}
       renderOption={(props, option) => <li {...props}>{option.title}</li>}
       sx={{ width: { xs: '100%', md: '60%' } }}
       freeSolo
