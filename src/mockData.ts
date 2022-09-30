d58a8c6467d5123e6908c092bcf2d029fa725fce
Stanislav Klymoshenko <s.klymoshenko@gmail.com>
2022-09-30 16:48:33 +0200

    fix data generator

--- src/mockData.ts
+++ src/mockData.ts
@@ -22,12 +22,19 @@ const titles = [
 export const generateProductOptions = (): ProductOptionType[] => {
   return titles.map((title, i) => {
     const todayDay = DateTime.now().day > 2 ? DateTime.now().day : 2
+    const dateDay =
+      todayDay >= 30 ? chance.integer({ min: 1, max: 30 }) : todayDay + chance.integer({ min: 1, max: 30 - todayDay })
+    let month = DateTime.now().month - 1
+
+    if (todayDay >= 30) {
+      month = chance.pickone([true, false]) ? DateTime.now().month : DateTime.now().month - 1
+    }
     const date = new Date(
       chance.date({
         string: true,
         year: DateTime.now().year,
-        month: DateTime.now().month - 1,
-        day: todayDay + chance.integer({ min: 1, max: 30 - todayDay }),
+        month: month,
+        day: dateDay,
       }),
     ).toISOString()
 
